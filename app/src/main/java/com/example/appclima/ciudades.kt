512c17edf9990159b525c1166f3a4000835e3ab4
EmilyBalam <72897197+EmilyBalam@users.noreply.github.com>
2022-02-21 15:18:10 -0600

    SegundoActivity_Utilizando_Recursos

---
+++ app/src/main/java/com/example/appclima/ciudades.kt
@@ -0,0 +1,11 @@
+package com.example.appclima
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ciudades : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ciudades)
+    }
+}
