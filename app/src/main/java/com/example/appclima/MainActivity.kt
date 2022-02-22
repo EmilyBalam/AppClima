315c622ba0f105632b1b5482de3db946412e6e9f
EmilyBalam <72897197+EmilyBalam@users.noreply.github.com>
2022-02-21 19:19:09 -0600

    Aplicacion del clima terminada

--- app/src/main/java/com/example/appclima/MainActivity.kt
+++ app/src/main/java/com/example/appclima/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.appclima
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var tvciudad:TextView?=null
+    var tvgrados:TextView?=null
+    var tvestatus:TextView?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        tvciudad= findViewById(R.id.tvCiudad)
+        tvgrados=findViewById(R.id.tvGrados)
+        tvestatus=findViewById(R.id.tvEstatus)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+         val ciudad = intent.getStringExtra("com.vidamrr.appclima.ciudades.ciudad" )
+
+        val ciudadFCP = Ciudad("Felipe Carrillo Puerto", 27,"Despejado")
+        val ciudadPuertoM = Ciudad("Puerto Morelos", 30,"Soleado")
+        val ciudadJoseMM = Ciudad("Jose Maria Morelos", 10,"Nublado")
+        val ciudadChe = Ciudad("Chetumal", 18,"Lloviendo")
+
+        if(ciudad == "Ciudad-felipeCP"){
+            tvciudad?.text= ciudadFCP.nombre
+            tvgrados?.text= ciudadFCP.grados.toString() + "°"
+            tvestatus?.text = ciudadFCP.estatus
+
+        }else if(ciudad == "Ciudad-PuertoMorelos"){
+            tvciudad?.text= ciudadPuertoM.nombre
+            tvestatus?.text = ciudadPuertoM.estatus
+            tvgrados?.text= ciudadPuertoM.grados.toString() + "°"
+
+        }else if(ciudad =="Ciudad-JoseMariaMorelos"){
+            tvciudad?.text= ciudadJoseMM.nombre
+            tvestatus?.text = ciudadJoseMM.estatus
+            tvgrados?.text= ciudadJoseMM.grados.toString() + "°"
+
+        }else if(ciudad == "Ciudad-Chetumal"){
+            tvciudad?.text= ciudadChe.nombre
+            tvestatus?.text = ciudadChe.estatus
+            tvgrados?.text= ciudadChe.grados.toString() + "°"
+
+        }else {
+            Toast.makeText(this,"No se encontro la informacion",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
--- app/src/main/java/com/example/appclima/ciudades.kt
+++ app/src/main/java/com/example/appclima/ciudades.kt
@@ -1,11 +1,44 @@
 package com.example.appclima
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class ciudades : AppCompatActivity() {
+
+    val TAG = "com.vidamrr.appclima.ciudades.ciudad"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ciudades)
+
+        val bFelipe= findViewById<Button>(R.id.bFCP)
+        val bPMorelos= findViewById<Button>(R.id.bPMorelos)
+        val bJMM= findViewById<Button>(R.id.bJMMorelos)
+        val bChetumal= findViewById<Button>(R.id.bChetumal)
+
+        bFelipe.setOnClickListener(View.OnClickListener {
+            val intent = Intent( this, MainActivity:: class.java)
+            intent.putExtra(TAG,"Ciudad-felipeCP")
+            startActivity(intent)
+        })
+        bPMorelos.setOnClickListener(View.OnClickListener {
+            val intent = Intent( this, MainActivity:: class.java)
+            intent.putExtra(TAG,"Ciudad-PuertoMorelos")
+            startActivity(intent)
+        })
+        bJMM.setOnClickListener(View.OnClickListener {
+            val intent = Intent( this, MainActivity:: class.java)
+            intent.putExtra(TAG,"Ciudad-JoseMariaMorelos")
+            startActivity(intent)
+        })
+        bChetumal.setOnClickListener(View.OnClickListener {
+            val intent = Intent( this, MainActivity:: class.java)
+            intent.putExtra(TAG,"Ciudad-Chetumal")
+            startActivity(intent)
+        })
+
+
     }
 }
